076351665bde069db9a39bf99a11a4dc464c709f
Prokop Randáček <prokop@randacek.dev>
2021-05-22 11:17:15 +0200

    place substations for beacons

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from settings import *
 x = 0
 foundFluid = False
 found6Ingr = ""
+lastNumberOfSubstations = 0
 
 
 def ratioCalc(da, r):
@@ -13,18 +14,25 @@ def ratioCalc(da, r):
 
 
 def buildBP(r, y=0, n=0, px=0, space=""):
-    global found6Ingr, foundFluid, x  # x is global, no matter how deep down
+    global found6Ingr, foundFluid, lastNumberOfSubstations, x  # x is global, no matter how deep down
     myx = x
     if r[0] in recipes.keys():  # only if there is a recipe for this item
         x += 9 + gap  # move to the left
+
         placeBusLink(-x, y, n)  # Connect to the bus
         placeBusLine(-x, y, n, myx - px)  # Extend the bus line
         assemblerNum = ratioCalc(r[1], r[0])  # calculate how many assemblers we need
         for i in range(assemblerNum):
             placeAssemblerUnit(-x, y + i * 3, r[0])  # place assemblers
         placeBeaconEnd(-x, y + (assemblerNum - 1) * 3)  # place beacons
-        for i in range(int(ceil(assemblerNum * 3 / 18))):
+        numberOfSubstations = int(ceil(assemblerNum * 3 / 18))
+        for i in range(numberOfSubstations):
             placeSubstation(-x - 6, y + i * 18)  # place substations
+            if useBeacon and i >= lastNumberOfSubstations:
+                placeSubstation(-x + 8, y + i * 18)  # place substations
+
+        lastNumberOfSubstations = numberOfSubstations
+
         nn = 0
         if len(recipes[r[0]]) > 6:
             found6Ingr = r[0]  # 6 ingredients are not supported yet
@@ -39,6 +47,7 @@ def buildBP(r, y=0, n=0, px=0, space=""):
             else:  # it is normal recipe that we can create -> recursive call itself on that recipe
                 buildBP([i[0], r[1] * i[1]], y + 3, n=nn, px=myx, space="  " + space)
                 nn += 1
+
         wasLastManual = False
         for m in manual:  # manual inputs are connected last
             placeManualInput(-myx - 11, y, nn, m, wasLastManual)
@@ -50,6 +59,7 @@ def GenBP(item, ips):  # ips = items per second
     global bp, x, foundFluid, found6Ingr
     # reset stuff
     x = 0
+    lastNumberOfSubstations = 0
     foundFluid = False
     found6Ingr = ""
     bp.reset()
